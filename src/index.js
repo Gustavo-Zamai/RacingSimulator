f11f30fd0215d81a6973cd82c241d228db78b5b0
Gustavo Zamai <gustavosimaozamai@gmail.com>
2025-02-04 14:51:16 -0300

    Add power ups and a check to be different characters

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 // players objects
 characters = [
-    {name: "Mario", speed: 4, handling: 3, power: 3, score: 0},
-    {name: "Luigi", speed: 3, handling: 4, power: 3, score: 0},
-    {name: "Peach", speed: 3, handling: 4, power: 2, score: 0},
-    {name: "Donkey Kong", speed: 2, handling: 2, power: 5, score: 0},
-    {name: "Toad", speed: 2, handling: 5, power: 4, score: 0},
-    {name: "Yoshi", speed: 2, handling: 4, power: 3, score: 0},
-    {name: "Bowser", speed: 5, handling: 2, power: 5, score: 0},
-    {name: "Kirby", speed: 5, handling: 3, power: 2, score: 0},
-    {name: "Samus", speed: 4, handling: 4, power: 4, score: 0},
-    {name: "Fox", speed: 3, handling: 5, power: 3, score: 0},
+    { name: "Mario", speed: 4, handling: 3, power: 3, score: 0 },
+    { name: "Luigi", speed: 3, handling: 4, power: 3, score: 0 },
+    { name: "Peach", speed: 3, handling: 4, power: 2, score: 0 },
+    { name: "Donkey Kong", speed: 2, handling: 2, power: 5, score: 0 },
+    { name: "Toad", speed: 2, handling: 5, power: 4, score: 0 },
+    { name: "Yoshi", speed: 2, handling: 4, power: 3, score: 0 },
+    { name: "Bowser", speed: 5, handling: 2, power: 5, score: 0 },
+    { name: "Kirby", speed: 5, handling: 3, power: 2, score: 0 },
+    { name: "Samus", speed: 4, handling: 4, power: 4, score: 0 },
+    { name: "Fox", speed: 3, handling: 5, power: 3, score: 0 },
 ];
 
 async function rollDice() {
@@ -97,6 +97,9 @@ async function playRace(characterOne, characterTwo) {
             let powerResultOne = (characterOne.power + diceOne);
             let powerResultTwo = (characterTwo.power + diceTwo);
 
+            let powerUps = ['Turtle Shell', 'Bomb'];
+            let powerUp = powerUps[Math.floor(Math.random() * powerUps.length)];
+
             console.log(`${characterOne.name} battle with ${characterTwo.name} 🥊`);
 
             await logRollResult(
@@ -113,14 +116,30 @@ async function playRace(characterOne, characterTwo) {
                 characterTwo.power
             );
 
-            if(powerResultOne > powerResultTwo && characterTwo.score > 0) {
-                console.log(`${characterOne.name} wins the battle! ${characterTwo.name} losses one point 🐢`)
-                characterTwo.score--;
+            if (powerResultOne > powerResultTwo && characterTwo.score > 0) {
+                if (powerUp === "Turtle Shell") {
+                    console.log(`${characterOne.name} wins the battle! ${characterTwo.name} was hit by a ${powerUp} and losses 2 points 🐢`)
+                    characterTwo.score -= 2;
+                } else if (powerUp === "Bomb") {
+                    console.log(`${characterOne.name} wins the battle! ${characterTwo.name} was blows away by a ${powerUp} and losses 3 points 💣`)
+                    characterTwo.score -= 3;
+                } else {
+                    console.log(`${characterOne.name} wins the battle! ${characterTwo.name} losses one point 🐢`)
+                    characterOne.score--;
+                }
             }
 
-            if(powerResultTwo > powerResultOne && characterOne.score > 0) {
-                console.log(`${characterTwo.name} wins the battle! ${characterOne.name} losses one point 🐢`)
-                characterOne.score--;
+            if (powerResultTwo > powerResultOne && characterOne.score > 0) {
+                if (powerUp === "Turtle Shell") {
+                    console.log(`${characterTwo.name} wins the battle! ${characterOne.name} was hit by a ${powerUp} and losses 2 points 🐢`)
+                    characterOne.score -= 2;
+                } else if (powerUp === "Bomb") {
+                    console.log(`${characterTwo.name} wins the battle! ${characterOne.name} was blows away by a ${powerUp} and losses 3 points 💣`)
+                    characterOne.score -= 3;
+                } else {
+                    console.log(`${characterTwo.name} wins the battle! ${characterOne.name} losses one point 🐢`)
+                    characterOne.score--;
+                }
             }
             /* ifs ternarios
             characterTwo.score -= powerResultOne > powerResultTwo && characterTwo.score > 0 ? 1 : 0;
@@ -133,6 +152,7 @@ async function playRace(characterOne, characterTwo) {
                 : "");
             */
         }
+        let nitro = Math.floor(Math.random() * 1);
 
         // compare skills
         if (totalTestSkillOne > totalTestSkillTwo) {
@@ -141,16 +161,15 @@ async function playRace(characterOne, characterTwo) {
         } else if (totalTestSkillOne < totalTestSkillTwo) {
             console.log(`${playerTwo.name} wins this round! \n`);
             characterTwo.score++;
-        } 
-
+        }
         console.log("----------------------------------------------------------------");
     }
 };
 
-async function declareWinner(playerOne, PlayerTwo) {
+async function declareWinner(playerOne, playerTwo) {
     console.log(`Final score \n`);
-    console.log(`${playerOne.name}: ${playerOne.score} point(s)`);
-    console.log(`${playerTwo.name}: ${playerTwo.score} point(s)`);
+    console.log(`${playerOne.name}: ${playerOne.score} point(s) \n`);
+    console.log(`${playerTwo.name}: ${playerTwo.score} point(s) \n`);
 
     if (playerOne.score > playerTwo.score) {
         console.log(`${playerOne.name} wins the race! 🏆`);
@@ -166,14 +185,19 @@ async function declareWinner(playerOne, PlayerTwo) {
     let randomNumber1 = Math.floor(Math.random() * characters.length);
     let randomNumber2 = Math.floor(Math.random() * characters.length);
 
-    console.log(randomNumber1, randomNumber2);
-    playerOne = characters[randomNumber1];
-    playerTwo = characters[randomNumber2];
-    console.log(playerOne, playerTwo);
-    console.log(
-        `🚨 🏁 Racing between ${playerOne.name} and ${playerTwo.name} is about to begin... \n`
-    );
+    if (randomNumber1 !== randomNumber2) {
 
-    await playRace(playerOne, playerTwo);
-    await declareWinner(playerOne, playerTwo);
+        console.log(randomNumber1, randomNumber2);
+        playerOne = characters[randomNumber1];
+        playerTwo = characters[randomNumber2];
+        console.log(playerOne, playerTwo);
+        console.log(
+            `🚨 🏁 Racing between ${playerOne.name} and ${playerTwo.name} is about to begin... \n`
+        );
+
+        await playRace(playerOne, playerTwo);
+        await declareWinner(playerOne, playerTwo);
+    }else {
+        console.log("Bad mood outside for a race! 🌧️");
+    }
 })();
